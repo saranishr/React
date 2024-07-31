c864fc3c4b4ffd95991b72874258a5a960e742c2
saranishr <142418625+saranishr@users.noreply.github.com>
2024-07-31 15:05:26 +0530

    Add files via upload

---
+++ src/Day1.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+const alertMsg=()=>
+{
+    alert("Welcome");
+    console.log("React Application");
+}
+const Day1=()=>
+{
+    return (
+        <div>
+            <button onClick={alertMsg}>Click Me</button>
+        </div>
+    )
+}
+
+
+
+export default Day1 ;
